0d642c68647a41de0c9d1b4e20f8ed3db24edc31
Gabriel Cedraz <cedrazdev@gmail.com>
2025-03-18 20:15:43 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create server instance
+const server = new McpServer({
+  name: "teste-cedraz",
+  version: "1.0.0",
+});
+
+// Add an addition tool
+server.tool("add",
+  { a: z.number(), b: z.number() },
+  async ({ a, b }) => ({
+    content: [{ type: "text", text: `The sum of ${a} and ${b} is ${a + b}` }]
+  })
+);
+
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
